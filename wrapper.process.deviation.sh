36aa4eea062f2f4745f0bcbeba01a479ef1b3d1c
Joseph Oravetz <jjoravet@gmail.com>
2016-08-03 13:05:52 -0500

    Added some documentation to the wrapper script to loop over well deviations

--- wrapper.process.deviation.sh
+++ wrapper.process.deviation.sh
@@ -1,3 +1,8 @@
+#! /bin/bash
+
+### wrapper script to  loop over each deviation survey - stored in deviations.lis ###
+
+ls -alt *deviation_survey_from_Petrel.dat | awk '{print $9}' > deviations.lis
 while read -r LINE ; do
    process.deviation.sh -s ${LINE}
 done < deviations.lis
